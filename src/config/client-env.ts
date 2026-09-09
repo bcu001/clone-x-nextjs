712e4d0facded1b90c14609e2f7e080c6a702581
Bhuwan Upadhyay <bhuwan.upadhyay.work@gmail.com>
2026-09-09 18:37:57 +0530

    feat: added shadcn ui and made env config for client and server:

---
+++ src/config/client-env.ts
@@ -0,0 +1,6 @@
+const CLIENT_ENV = {
+    urlEndPoint : process.env.NEXT_PUBLIC_URL_ENDPOINT,
+    imagekit_public_key: process.env.NEXT_PUBLIC_PUBLIC_KEY
+}
+
+export default CLIENT_ENV;
---
+++ src/config/server-env.ts
@@ -0,0 +1,7 @@
+import "server-only"
+
+const SERVER_ENV = {
+    imagekit_secret_key : process.env.PRIVATE_KEY,
+}
+
+export default SERVER_ENV;
